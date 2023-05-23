dbfa57417ff0a47b9bbb6566e9c8a9271c51241b
Vallab Karanam <k.vallab6@gmail.com>
2023-05-22 23:32:46 -0700

    Implementing latest React Router

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
 
 const HomeScreen = () => {
   return (
-    <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </>
+    <main className="py-3">
+      <Container>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </main>
   )
 }
 
