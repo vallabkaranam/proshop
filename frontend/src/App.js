b96e8c41a75d183b3ef876ff66d60732005a3d75
Vallab Karanam <k.vallab6@gmail.com>
2023-06-22 23:27:47 -0700

    update App.js and index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/product/:id" element={<ProductScreen />} />
-      </Routes>
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
       <Footer />
-    </Router>
+    </>
   )
 }
 
