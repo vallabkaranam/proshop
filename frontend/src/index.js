e24af405ddc1ffb9d4616dd41d5e90e216b8867b
Vallab Karanam <k.vallab6@gmail.com>
2023-06-25 18:06:36 -0700

    redux store setup and generic API slice setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
 
