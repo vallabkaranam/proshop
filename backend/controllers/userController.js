4e0c0707b00e9e6ec3fc84c22499809fdac94afa
Vallab Karanam <k.vallab6@gmail.com>
2023-09-27 21:33:23 -0700

    logout user & clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,14 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('logout user')
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
+  res.status(200).json({
+    message: 'Logged out successfully',
+  })
 })
 
 // no need to pass in id because we can get the id from their own token
