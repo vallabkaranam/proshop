fc52f82a9c23c7d5439ed86f5aa3087e64ceee0c
Vallab Karanam <k.vallab6@gmail.com>
2023-09-30 18:07:07 -0700

    creating an order

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -43,6 +43,12 @@ const cartSlice = createSlice({
 
       return updateCart(state)
     },
+
+    clearCartItems: (state, action) => {
+      state.cartItems = []
+
+      return updateCart(state)
+    },
   },
 })
 
@@ -51,6 +57,7 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
 } = cartSlice.actions
 
 export default cartSlice.reducer
