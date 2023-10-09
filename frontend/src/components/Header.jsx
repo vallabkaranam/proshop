ebeb0f59b0bd3ee6c46527eeda5e733cad7dd89f
Vallab Karanam <k.vallab6@gmail.com>
2023-10-08 22:04:24 -0700

    search box component and clear search (go back to home) button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useLogoutMutation } from '../slices/usersApiSlice'
 import { logout } from '../slices/authSlice'
+import SearchBox from './SearchBox'
 import logo from '../assets/logo.png'
 
 import { useSelector, useDispatch } from 'react-redux'
@@ -40,6 +41,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+              <SearchBox />
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
