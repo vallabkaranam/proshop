398d77a14de97ede7b447443f189366c6e63b5df
Vallab Karanam <k.vallab6@gmail.com>
2023-10-08 21:44:58 -0700

    search products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,12 +5,22 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 4
+  const pageSize = 8
   const page = Number(req.query.pageNumber) || 1
-  // will get total number of products
-  const count = await Product.countDocuments()
 
-  const products = await Product.find({})
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i', // case insensitive
+        },
+      }
+    : {}
+
+  // will get total number of products
+  const count = await Product.countDocuments({ ...keyword })
+
+  const products = await Product.find({ ...keyword })
     .limit(pageSize)
     // skip the previous page's products
     .skip(pageSize * (page - 1))
