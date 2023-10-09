3b5573b073016d3a230886e942420bd6a1a7a248
Vallab Karanam <k.vallab6@gmail.com>
2023-10-08 21:32:28 -0700

    paginate component

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 2
+  const pageSize = 4
   const page = Number(req.query.pageNumber) || 1
   // will get total number of products
   const count = await Product.countDocuments()
